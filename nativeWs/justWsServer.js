b26ca25cc732899b40529a630aaf3c5a637b84cd
myungwoo-Y <auddn6676@naver.com>
2019-09-29 10:44:22 +0900

    add bi-interaction about client and server

--- nativeWs/justWsServer.js
+++ nativeWs/justWsServer.js
@@ -6,4 +6,16 @@ const server = http.createServer((req, res) => {
     res.end("I am connected!")
 });
 
+const wss = new websocket.Server({server});
+wss.on('headers', (headers, req) => {
+    console.log(headers);
+})
+
+wss.on('connection', (ws, req)=>{
+    ws.send('welcome to the websocket server!!')
+    ws.on('message', (msg) => {
+        console.log(msg)
+    })
+})
+
 server.listen(8000);
